0f025a374fe6ce655ebfdf3e83dcec3bdb9d5813
Aravind Umasankar <aravind-umasankar@hpe.com>
2022-05-13 10:02:49 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -31,11 +31,11 @@ import object_detection
 
 logging.basicConfig(filename='app.log')
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
 
 # create console handler and set level to debug
 ch = logging.StreamHandler()
-ch.setLevel(logging.DEBUG)
+ch.setLevel(logging.INFO)
 
 # create formatter
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
