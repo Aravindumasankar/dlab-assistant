3624a8ad114fc2e2b4e876250fc912f3c2371966
Aravind Umasankar <aravind-umasankar@hpe.com>
2022-05-13 09:30:00 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -25,6 +25,7 @@ import ocr
 from datetime import date, datetime
 from functools import wraps
 from flask import Flask, request, jsonify, abort, send_from_directory, send_file
+from flask_cors import CORS
 import json
 import wget
 import object_detection
@@ -32,6 +33,7 @@ import object_detection
 logging.basicConfig(filename='app.log')
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
+
 
 # create console handler and set level to debug
 ch = logging.StreamHandler()
@@ -60,6 +62,7 @@ except:
     print("Could not connect to MongoDB")
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/meme/*": {"origins": "*"}})
 
 
 # The actual decorator function check api key
